63c4352ab7582d9d1f80933a58af1e0106a64b46
Vasili Vasilyeu <vasili.v@tut.by>
2017-06-16 12:02:04 +0300

    Added insert for IPv6 network

--- iptree/iptree.go
+++ iptree/iptree.go
@@ -1,17 +1,22 @@
-// Package iptree implements radix tree data structure for IPv4 networks.
+// Package iptree implements radix tree data structure for IPv4 and IPv6 networks.
 package iptree
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/infobloxopen/go-trees/numtree"
 )
 
-const iPv4Bits = net.IPv4len * 8
+const (
+	iPv4Bits = net.IPv4len * 8
+	iPv6Bits = net.IPv6len * 8
+)
 
-// Tree is a radix tree for IPv4 networks.
+// Tree is a radix tree for IPv4 and IPv6 networks.
 type Tree struct {
-	root *numtree.Node32
+	root32 *numtree.Node32
+	root64 *numtree.Node64
 }
 
 // NewTree creates empty tree.
@@ -27,7 +32,31 @@ func (t *Tree) InsertNet(n *net.IPNet, value interface{}) *Tree {
 
 	if key, bits := iPv4NetToUint32(n); bits >= 0 {
 		return &Tree{
-			root: t.root.Insert(key, bits, value)}
+			root32: t.root32.Insert(key, bits, value),
+			root64: t.root64}
+	}
+
+	if MSKey, MSBits, LSKey, LSBits := iPv6NetToUint64Pair(n); MSBits >= 0 {
+		if MSBits < 64 {
+			return &Tree{
+				root32: t.root32,
+				root64: t.root64.Insert(MSKey, MSBits, value)}
+		}
+
+		var r *numtree.Node64
+		if v, ok := t.root64.ExactMatch(MSKey, MSBits); ok {
+			r, ok = v.(*numtree.Node64)
+			if !ok {
+				err := fmt.Errorf("invalid IPv6 tree: expected *numtree.Node64 value at 0x%016x, %d but got %T (%#v)",
+					MSKey, MSBits, v, v)
+				panic(err)
+			}
+		}
+
+		r = r.Insert(LSKey, LSBits, value)
+		return &Tree{
+			root32: t.root32,
+			root64: t.root64.Insert(MSKey, MSBits, r)}
 	}
 
 	return t
@@ -44,4 +73,30 @@ func iPv4NetToUint32(n *net.IPNet) (uint32, int) {
 	}
 
 	return (uint32(n.IP[0]) << 24) | (uint32(n.IP[1]) << 16) | (uint32(n.IP[2]) << 8) | uint32(n.IP[3]), ones
+}
+
+func iPv6NetToUint64Pair(n *net.IPNet) (uint64, int, uint64, int) {
+	if len(n.IP) != net.IPv6len {
+		return 0, -1, 0, -1
+	}
+
+	ones, bits := n.Mask.Size()
+	if bits != iPv6Bits {
+		return 0, -1, 0, -1
+	}
+
+	MSBits := 64
+	LSBits := 0
+	if ones > 64 {
+		LSBits = ones - 64
+	} else {
+		MSBits = ones
+	}
+
+	return packIPToUint64(n.IP), MSBits, packIPToUint64(n.IP[8:]), LSBits
+}
+
+func packIPToUint64(x net.IP) uint64 {
+	return (uint64(x[0]) << 56) | (uint64(x[1]) << 48) | (uint64(x[2]) << 40) | (uint64(x[3]) << 32) |
+		(uint64(x[4]) << 24) | (uint64(x[5]) << 16) | (uint64(x[6]) << 8) | uint64(x[7])
 }
--- iptree/iptree_test.go
+++ iptree/iptree_test.go
@@ -3,6 +3,8 @@ package iptree
 import (
 	"net"
 	"testing"
+
+	"github.com/infobloxopen/go-trees/numtree"
 )
 
 func TestInsertNet(t *testing.T) {
@@ -10,19 +12,53 @@ func TestInsertNet(t *testing.T) {
 
 	newR := r.InsertNet(nil, "test")
 	if newR != r {
-		t.Errorf("Expected no changes inserting nil network but got:\n%s\n", newR.root.Dot())
+		t.Errorf("Expected no changes inserting nil network but got:\n%s\n", newR.root32.Dot())
 	}
 
 	newR = r.InsertNet(&net.IPNet{IP: nil, Mask: nil}, "test")
 	if newR != r {
-		t.Errorf("Expected no changes inserting invalid network but got:\n%s\n", newR.root.Dot())
+		t.Errorf("Expected no changes inserting invalid network but got:\n%s\n", newR.root32.Dot())
 	}
 
 	_, n, _ := net.ParseCIDR("192.0.2.0/24")
 	newR = r.InsertNet(n, "test")
 	if newR == r {
-		t.Errorf("Expected new root after insertion of new address but got previous")
+		t.Errorf("Expected new root after insertion of new IPv4 address but got previous")
+	} else {
+		assertTree32Node(newR, 0xc0000200, 24, "test", "tree with single IPv4 address inserted", t)
 	}
+
+	_, n, _ = net.ParseCIDR("2001:db8::/32")
+	r1 := r.InsertNet(n, "test")
+	if r1 == r {
+		t.Errorf("Expected new root after insertion of new IPv6 address but got previous")
+	} else {
+		assertTree64Node(r1, 0x20010db800000000, 32, 0x0, 0, "test",
+			"tree with single IPv6 address inserted", t)
+	}
+
+	_, n, _ = net.ParseCIDR("2001:db8:0:0:0:ff::/96")
+	r2 := r1.InsertNet(n, "test 1")
+	if r2 == r1 {
+		t.Errorf("Expected new root after insertion of second IPv6 address but got previous")
+	} else {
+		assertTree64Node(r2, 0x20010db800000000, 64, 0x000000ff00000000, 32, "test 1",
+			"tree with second IPv6 address inserted", t)
+	}
+
+	_, n, _ = net.ParseCIDR("2001:db8:0:0:0:fe::/96")
+	r3 := r2.InsertNet(n, "test 2")
+	if r3 == r1 {
+		t.Errorf("Expected new root after insertion of third IPv6 address but got previous")
+	} else {
+		assertTree64Node(r3, 0x20010db800000000, 64, 0x000000fe00000000, 32, "test 2",
+			"tree with third IPv6 address inserted", t)
+	}
+
+	invR := NewTree()
+	invR.root64 = invR.root64.Insert(0x20010db800000000, 64, "test")
+	_, n, _ = net.ParseCIDR("2001:db8:0:0:0:ff::/96")
+	assertPanic(func() { invR.InsertNet(n, "panic") }, "inserting to invalid IPv6 tree", t)
 }
 
 func TestIPv4NetToUint32(t *testing.T) {
@@ -33,11 +69,11 @@ func TestIPv4NetToUint32(t *testing.T) {
 	}
 
 	n = &net.IPNet{
-		IP:   net.IP{0xc, 0x00, 0x02},
+		IP:   net.IP{0xc, 0x00},
 		Mask: net.IPMask{0xff, 0xff, 0xff, 0x00}}
 	key, bits = iPv4NetToUint32(n)
 	if bits >= 0 {
-		t.Errorf("Expected negative number of bits but got %d", bits)
+		t.Errorf("Expected negative number of bits for invalid IPv4 address but got 0x%08x, %d", key, bits)
 	}
 
 	n = &net.IPNet{
@@ -45,6 +81,122 @@ func TestIPv4NetToUint32(t *testing.T) {
 		Mask: net.IPMask{0xff, 0x00, 0xff, 0x00}}
 	key, bits = iPv4NetToUint32(n)
 	if bits >= 0 {
-		t.Errorf("Expected negative number of bits but got %d", bits)
+		t.Errorf("Expected negative number of bits for invalid IPv4 mask but got 0x%08x, %d", key, bits)
 	}
+}
+
+func TestIPv6NetToUint64Pair(t *testing.T) {
+	_, n, _ := net.ParseCIDR("2001:db8::/32")
+	MSKey, MSBits, LSKey, LSBits := iPv6NetToUint64Pair(n)
+	if MSKey != 0x20010db800000000 || MSBits != 32 || LSKey != 0x0 || LSBits != 0 {
+		t.Errorf("Expected 0x20010db800000000, 32 and 0x0000000000000000, 0 pairs bit got 0x%016x, %d and 0x%016x, %d",
+			MSKey, MSBits, LSKey, LSBits)
+	}
+
+	_, n, _ = net.ParseCIDR("2001:db8:0:0:0:ff::/96")
+	MSKey, MSBits, LSKey, LSBits = iPv6NetToUint64Pair(n)
+	if MSKey != 0x20010db800000000 || MSBits != 64 || LSKey != 0x000000ff00000000 || LSBits != 32 {
+		t.Errorf("Expected 0x20010db800000000, 32 and 0x0000000000000000, 0 pairs bit got 0x%016x, %d and 0x%016x, %d",
+			MSKey, MSBits, LSKey, LSBits)
+	}
+
+	n = &net.IPNet{
+		IP: net.IP{
+			0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00},
+		Mask: net.IPMask{
+			0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
+	MSKey, MSBits, LSKey, LSBits = iPv6NetToUint64Pair(n)
+	if MSBits >= 0 {
+		t.Errorf("Expected negative number of bits for invalid IPv6 address but got 0x%016x, %d and 0x%016x, %d",
+			MSKey, MSBits, LSKey, LSBits)
+	}
+
+	n = &net.IPNet{
+		IP: net.IP{
+			0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		Mask: net.IPMask{
+			0x00, 0xff, 0x00, 0xff, 0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
+	MSKey, MSBits, LSKey, LSBits = iPv6NetToUint64Pair(n)
+	if MSBits >= 0 {
+		t.Errorf("Expected negative number of bits for invalid IPv6 mask but got 0x%016x, %d and 0x%016x, %d",
+			MSKey, MSBits, LSKey, LSBits)
+	}
+}
+
+func assertTree32Node(r *Tree, key uint32, bits int, e, desc string, t *testing.T) {
+	v, ok := r.root32.ExactMatch(key, bits)
+	if ok {
+		s, ok := v.(string)
+		if ok {
+			if s != e {
+				t.Errorf("Expected string %q at 0x%08x, %d for %s but got %q", e, key, bits, desc, s)
+			}
+		} else {
+			t.Errorf("Expected string %q at 0x%08x, %d for %s but got %T (%#v)", e, key, bits, desc, v, v)
+		}
+	} else {
+		t.Errorf("Expected string %q at 0x%08x, %d for %s but got nothing", e, key, bits, desc)
+	}
+}
+
+func assertTree64Node(r *Tree, MSKey uint64, MSBits int, LSKey uint64, LSBits int, e, desc string, t *testing.T) {
+	v, ok := r.root64.ExactMatch(MSKey, MSBits)
+	if ok {
+		if MSBits < 64 {
+			s, ok := v.(string)
+			if ok {
+				if s != e {
+					t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got %q",
+						e, MSKey, MSBits, LSKey, LSBits, desc, s)
+				}
+			} else {
+				t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got %T (%#v)",
+					e, MSKey, MSBits, LSKey, LSBits, desc, v, v)
+			}
+		} else {
+			r, ok := v.(*numtree.Node64)
+			if ok {
+				v, ok := r.ExactMatch(LSKey, LSBits)
+				if ok {
+					s, ok := v.(string)
+					if ok {
+						if s != e {
+							t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got %q",
+								e, MSKey, MSBits, LSKey, LSBits, desc, s)
+						}
+					} else {
+						t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got %T (%#v)",
+							e, MSKey, MSBits, LSKey, LSBits, desc, v, v)
+					}
+				} else {
+					t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got nothing at second hop",
+						e, MSKey, MSBits, LSKey, LSBits, desc)
+				}
+			} else {
+				t.Errorf("Expected *numtree.Node64 at 0x%016x, %d and 0x%016x, %d for %s (first hop) but got %T (%#v)",
+					MSKey, MSBits, LSKey, LSBits, desc, v, v)
+			}
+		}
+	} else {
+		if MSBits < 64 {
+			t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got nothing",
+				e, MSKey, MSBits, LSKey, LSBits, desc)
+		} else {
+			t.Errorf("Expected string %q at 0x%016x, %d and 0x%016x, %d for %s but got nothing even at first hop",
+				e, MSKey, MSBits, LSKey, LSBits, desc)
+		}
+	}
+}
+
+func assertPanic(f func(), desc string, t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic from %s but got nothing", desc)
+		}
+	}()
+
+	f()
 }
