b112a1f22c438767d51e43a4eccd3a3f7edbaada
Vasili Vasilyeu <vasili.v@tut.by>
2018-08-21 19:42:02 +0300

    Added domain package tuned for sdntable based domain map

---
+++ udomain/name.go
@@ -0,0 +1,262 @@
+// Package domain provides functions to parse and handle domain names.
+package domain
+
+import (
+	"encoding/binary"
+	"errors"
+	"math"
+)
+
+var (
+	// ErrLabelTooLong is returned when one of domain labels has more than 63 characters.
+	ErrLabelTooLong = errors.New("label too long")
+	// ErrEmptyLabel indicates that domain name contains empty label.
+	ErrEmptyLabel = errors.New("empty label")
+	// ErrNameTooLong is returned when domain name has more than 255 characters.
+	ErrNameTooLong = errors.New("name too long")
+	// ErrInvalidEscape is returned for invalid escape sequence.
+	ErrInvalidEscape = errors.New("invalid escape sequence")
+)
+
+// Name is a structure which represents domain name.
+type Name struct {
+	h string
+	n int
+	c []uint64
+}
+
+const (
+	// MaxName is maximum number of bytes for whole domain name.
+	MaxName = 255
+	// MaxLabels is maximum number of labels domain name can consist of.
+	MaxLabels = MaxName / 2
+	// MaxLabel is maximim number of bytes for single label.
+	MaxLabel = 63
+
+	uInt64Size = 8
+)
+
+const (
+	escRegular = iota
+	escFirstChar
+	escSecondDigit
+	escThirdDigit
+)
+
+func MakeNameFromString(s string) (Name, error) {
+	out := Name{h: s}
+	if len(s) <= 0 || s == "." {
+		return out, nil
+	}
+
+	var (
+		fragment [uInt64Size]byte
+		zeros    [uInt64Size]byte
+		label    [(MaxLabel + 1) / uInt64Size]uint64
+		name     [MaxLabels]uint64
+	)
+
+	n := 0
+	j := 1
+	k := 0
+	count := 1
+	esc := escRegular
+	code := 0
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		switch esc {
+		case escRegular:
+			switch b {
+			default:
+				if b >= 'a' && b <= 'z' {
+					b &= 0xdf
+				}
+
+				fragment[j] = b
+				j++
+
+				count++
+				if count > MaxName {
+					return out, ErrNameTooLong
+				}
+
+				if j >= len(fragment) {
+					if n >= len(label) {
+						return out, ErrLabelTooLong
+					}
+
+					label[n] = binary.LittleEndian.Uint64(fragment[:])
+					n++
+					j = 0
+				}
+
+			case '.':
+				if n < 1 && j <= 1 {
+					return out, ErrEmptyLabel
+				}
+
+				if j > 0 {
+					if n >= len(label) {
+						return out, ErrLabelTooLong
+					}
+
+					copy(fragment[j:], zeros[:])
+
+					label[n] = binary.LittleEndian.Uint64(fragment[:])
+					n++
+				}
+
+				label[0] |= uint64(n) | uint64(j<<4)
+
+				j = 1
+				count++
+
+				copy(name[k:], label[:n])
+				out.n++
+				k += n
+
+				n = 0
+				fragment[0] = 0
+
+			case '\\':
+				esc = escFirstChar
+			}
+
+		case escFirstChar:
+			if b < '0' || b > '9' {
+				esc = escRegular
+
+				if b >= 'a' && b <= 'z' {
+					b &= 0xdf
+				}
+
+				fragment[j] = b
+				j++
+
+				count++
+				if count > MaxName {
+					return out, ErrNameTooLong
+				}
+
+				if j >= len(fragment) {
+					if n >= len(label) {
+						return out, ErrLabelTooLong
+					}
+
+					label[n] = binary.LittleEndian.Uint64(fragment[:])
+					n++
+					j = 0
+				}
+
+				break
+			}
+
+			esc = escSecondDigit
+
+			code = int(b-'0') * 100
+			if code > math.MaxUint8 {
+				return out, ErrInvalidEscape
+			}
+
+		case escSecondDigit:
+			if b < '0' || b > '9' {
+				return out, ErrInvalidEscape
+			}
+
+			esc = escThirdDigit
+
+			code += int(b-'0') * 10
+			if code > math.MaxUint8 {
+				return out, ErrInvalidEscape
+			}
+
+		case escThirdDigit:
+			if b < '0' || b > '9' {
+				return out, ErrInvalidEscape
+			}
+
+			esc = escRegular
+
+			code += int(b - '0')
+			if code > math.MaxUint8 {
+				return out, ErrInvalidEscape
+			}
+
+			b = byte(code)
+			if b >= 'a' && b <= 'z' {
+				b &= 0xdf
+			}
+
+			fragment[j] = b
+			j++
+
+			count++
+			if count > MaxName {
+				return out, ErrNameTooLong
+			}
+
+			if j >= len(fragment) {
+				if n >= len(label) {
+					return out, ErrLabelTooLong
+				}
+
+				label[n] = binary.LittleEndian.Uint64(fragment[:])
+				n++
+				j = 0
+			}
+		}
+	}
+
+	if esc != escRegular {
+		return out, ErrInvalidEscape
+	}
+
+	if n > 0 && j > 0 || j > 1 {
+		if n >= len(label) {
+			return out, ErrLabelTooLong
+		}
+
+		copy(fragment[j:], zeros[:])
+
+		label[n] = binary.LittleEndian.Uint64(fragment[:])
+		n++
+
+		label[0] |= uint64(n) | uint64(j<<4)
+
+		copy(name[k:], label[:n])
+		out.n++
+		k += n
+	}
+
+	out.c = name[:k]
+	return out, nil
+}
+
+func MakeNameFromSlice(s []uint64) Name {
+	return Name{c: s}
+}
+
+func (n Name) String() string {
+	return n.h
+}
+
+func (n Name) Less(other Name) bool {
+	if len(n.c) < len(other.c) {
+		return true
+	}
+
+	if len(n.c) == len(other.c) {
+		for i, a := range n.c {
+			b := other.c[i]
+			if a < b {
+				return true
+			}
+
+			if a > b {
+				break
+			}
+		}
+	}
+
+	return false
+}
---
+++ udomain/name_test.go
@@ -0,0 +1,172 @@
+package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNameMakeNameFromString(t *testing.T) {
+	s := "looooooooooooooong.example.com"
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64{
+		// O O O O O O L (3 bytes in incomplete dword and 3 dwords for the label)
+		0x4f4f4f4f4f4f4c33,
+		// O O O O O O O O
+		0x4f4f4f4f4f4f4f4f,
+		//           G N O
+		0x0000000000474e4f,
+		// E L P M A X E (0 bytes in incomplete dword and 1 dword for the label)
+		0x454c504d41584501,
+		//         M O C (4 bytes in incomplete dword and 1 dword for the label)
+		0x000000004d4f4341,
+	}, n.c)
+}
+
+func TestNameMakeNameFromStringFQDN(t *testing.T) {
+	s := "looooooooooooooong.example.com."
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64{
+		// O O O O O O L (3 bytes in incomplete dword and 3 dwords for the label)
+		0x4f4f4f4f4f4f4c33,
+		// O O O O O O O O
+		0x4f4f4f4f4f4f4f4f,
+		//           G N O
+		0x0000000000474e4f,
+		// E L P M A X E (0 bytes in incomplete dword and 1 dword for the label)
+		0x454c504d41584501,
+		//         M O C (4 bytes in incomplete dword and 1 dword for the label)
+		0x000000004d4f4341,
+	}, n.c)
+}
+
+func TestNameMakeNameFromStringEmpty(t *testing.T) {
+	s := ""
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64(nil), n.c)
+}
+
+func TestNameMakeNameFromStringDot(t *testing.T) {
+	s := "."
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64(nil), n.c)
+}
+
+func TestNameMakeNameFromStringWithEscapedDot(t *testing.T) {
+	s := "www\\.example.com"
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64{
+		// A X E . W W W (4 bytes in incomplete dword and 2 dwords for the label)
+		0x4158452e57575742,
+		//         E L P M
+		0x00000000454c504d,
+		//         M O C (4 bytes in incomplete dword and 1 dword for the label)
+		0x000000004d4f4341,
+	}, n.c)
+}
+
+func TestNameMakeNameFromStringWithEscapedChar(t *testing.T) {
+	s := "www.e\\xample.com"
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64{
+		//         W W W (4 bytes in incomplete dword and 1 dword for the label)
+		0x0000000057575741,
+		// E L P M A X E (0 bytes in incomplete dword and 1 dword for the label)
+		0x454c504d41584501,
+		//         M O C (4 bytes in incomplete dword and 1 dword for the label)
+		0x000000004d4f4341,
+	}, n.c)
+}
+
+func TestNameMakeNameFromStringWithEscapedCode(t *testing.T) {
+	s := "www.e\\120ample.com"
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.h, "human-readable name should be the same as input string")
+	assert.Equal(t, []uint64{
+		//         W W W (4 bytes in incomplete dword and 1 dword for the label)
+		0x0000000057575741,
+		// E L P M A X E (0 bytes in incomplete dword and 1 dword for the label)
+		0x454c504d41584501,
+		//         M O C (4 bytes in incomplete dword and 1 dword for the label)
+		0x000000004d4f4341,
+	}, n.c)
+}
+
+func TestNameMakeNameFromStringWithNameTooLong(t *testing.T) {
+	s := "toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo." +
+		"loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong." +
+		"doooooooooooooooooooooooooooooooooooooooooooooooooooooooooomain." +
+		"naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaame"
+
+	n, err := MakeNameFromString(s)
+	assert.Equal(t, ErrNameTooLong, err, "name %q %08x", n, n.c)
+}
+
+func TestNameMakeNameFromStringWithTooLongLabel(t *testing.T) {
+	s := "www1.looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong.com"
+	n, err := MakeNameFromString(s)
+	assert.Equal(t, ErrLabelTooLong, err, "name %q %08x", n, n.c)
+
+	s = "www2.looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong.com"
+	n, err = MakeNameFromString(s)
+	assert.Equal(t, ErrLabelTooLong, err, "name %q %08x", n, n.c)
+
+	s = "www3.looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong"
+	n, err = MakeNameFromString(s)
+	assert.Equal(t, ErrLabelTooLong, err, "name %q %08x", n, n.c)
+}
+
+func TestNameMakeNameFromStringWithEmptyLabel(t *testing.T) {
+	s := "empty..label"
+	n, err := MakeNameFromString(s)
+	assert.Equal(t, ErrEmptyLabel, err, "name %q %08x", n, n.c)
+}
+
+func TestNameString(t *testing.T) {
+	s := "example.com"
+	n, err := MakeNameFromString(s)
+	assert.NoError(t, err)
+	assert.Equal(t, s, n.String(), "human-readable name should be the same as input string")
+}
+
+func TestNameLess(t *testing.T) {
+	n1, err := MakeNameFromString("example.com")
+	assert.NoError(t, err)
+
+	n2, err := MakeNameFromString("example.com")
+	assert.NoError(t, err)
+
+	assert.False(t, n1.Less(n2))
+	assert.False(t, n2.Less(n1))
+
+	a, err := MakeNameFromString("short.example.com")
+	assert.NoError(t, err)
+
+	b, err := MakeNameFromString("loooooong.example.com")
+	assert.NoError(t, err)
+
+	assert.True(t, a.Less(b))
+	assert.False(t, b.Less(a))
+
+	a, err = MakeNameFromString("example.com")
+	assert.NoError(t, err)
+
+	b, err = MakeNameFromString("example.net")
+	assert.NoError(t, err)
+
+	assert.True(t, a.Less(b))
+	assert.False(t, b.Less(a))
+}
