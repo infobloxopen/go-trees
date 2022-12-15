5008b8e2f35baa6315203cfa35307b0739defb3a
Slavcho Slavchev <sslavchev@infoblox.com>
2022-12-15 15:18:54 +0200

    Remove unused code

--- iptree/iptree.go
+++ iptree/iptree.go
@@ -18,38 +18,6 @@ var (
 	iPv6MaxMask = net.CIDRMask(iPv6Bits, iPv6Bits)
 )
 
-var (
-	masks32 = []uint32{
-		0x00000000, 0x80000000, 0xc0000000, 0xe0000000,
-		0xf0000000, 0xf8000000, 0xfc000000, 0xfe000000,
-		0xff000000, 0xff800000, 0xffc00000, 0xffe00000,
-		0xfff00000, 0xfff80000, 0xfffc0000, 0xfffe0000,
-		0xffff0000, 0xffff8000, 0xffffc000, 0xffffe000,
-		0xfffff000, 0xfffff800, 0xfffffc00, 0xfffffe00,
-		0xffffff00, 0xffffff80, 0xffffffc0, 0xffffffe0,
-		0xfffffff0, 0xfffffff8, 0xfffffffc, 0xfffffffe,
-		0xffffffff}
-
-	masks64 = []uint64{
-		0x0000000000000000, 0x8000000000000000, 0xc000000000000000, 0xe000000000000000,
-		0xf000000000000000, 0xf800000000000000, 0xfc00000000000000, 0xfe00000000000000,
-		0xff00000000000000, 0xff80000000000000, 0xffc0000000000000, 0xffe0000000000000,
-		0xfff0000000000000, 0xfff8000000000000, 0xfffc000000000000, 0xfffe000000000000,
-		0xffff000000000000, 0xffff800000000000, 0xffffc00000000000, 0xffffe00000000000,
-		0xfffff00000000000, 0xfffff80000000000, 0xfffffc0000000000, 0xfffffe0000000000,
-		0xffffff0000000000, 0xffffff8000000000, 0xffffffc000000000, 0xffffffe000000000,
-		0xfffffff000000000, 0xfffffff800000000, 0xfffffffc00000000, 0xfffffffe00000000,
-		0xffffffff00000000, 0xffffffff80000000, 0xffffffffc0000000, 0xffffffffe0000000,
-		0xfffffffff0000000, 0xfffffffff8000000, 0xfffffffffc000000, 0xfffffffffe000000,
-		0xffffffffff000000, 0xffffffffff800000, 0xffffffffffc00000, 0xffffffffffe00000,
-		0xfffffffffff00000, 0xfffffffffff80000, 0xfffffffffffc0000, 0xfffffffffffe0000,
-		0xffffffffffff0000, 0xffffffffffff8000, 0xffffffffffffc000, 0xffffffffffffe000,
-		0xfffffffffffff000, 0xfffffffffffff800, 0xfffffffffffffc00, 0xfffffffffffffe00,
-		0xffffffffffffff00, 0xffffffffffffff80, 0xffffffffffffffc0, 0xffffffffffffffe0,
-		0xfffffffffffffff0, 0xfffffffffffffff8, 0xfffffffffffffffc, 0xfffffffffffffffe,
-		0xffffffffffffffff}
-)
-
 // Tree is a radix tree for IPv4 and IPv6 networks.
 type Tree struct {
 	root32 *numtree.Node32
